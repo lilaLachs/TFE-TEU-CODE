ba28a57d11594a73269aa39b1b4ee7c7ffc92c22
MagicMaiki <129457363+MagicMaiki@users.noreply.github.com>
2023-03-31 00:55:56 +0200

    Delete fibonacci.h

--- Ackermann/fibonacci.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Maik Schmidt on 31.03.23.
-//
-
-int fibonacci_recursive(int nth_number);
-int fibonacci_iterative(int nth_number);
-int fibonacci(int nth_number);
-bool check_fibonacci(int nth_number, int expected);
